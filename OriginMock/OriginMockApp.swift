305099f7350423cfd1db9ce86d8552baedac659d
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-04-18 22:01:26 +0900

    Initial Commit

---
+++ OriginMock/OriginMockApp.swift
@@ -0,0 +1,20 @@
+//
+//  OriginMockApp.swift
+//  OriginMock
+//
+//  Created by Hiroaki-Hirabayashi on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct OriginMockApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
